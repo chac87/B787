939ccbad90884c364059286fb2384ebb1bb38ae7
Jonas Vaupel <jonasvaupel@MacBook-Pro-von-Jonas.local>
2026-02-10 08:31:24 +0100

    Quartz sync: Feb 10, 2026, 8:31 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,26 +25,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ContentMeta(),
     Component.TagList(),
   ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  left: [],
+  right: [],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
