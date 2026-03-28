b8add1dc7589a990babf73c6495e64465013dc41
Jonas Vaupel <vaupel.jonas@web.de>
2026-03-28 18:51:24 +0100

    Quartz sync: Mar 28, 2026, 6:51 PM

--- quartz/components/FloatingHome.tsx
+++ quartz/components/FloatingHome.tsx
@@ -11,7 +11,6 @@ export default (() => {
             <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
             <polyline points="9 22 9 12 15 12 15 22"/>
           </svg>
-          <span>Home</span>
         </a>
         <div class="floating-home__divider" aria-hidden="true"></div>
         <button class="floating-home__top" aria-label="Scroll to top">
